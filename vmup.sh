fe28316a055ab420ba32c7a2ddde48ae857df3af
mephistolist <49227141+mephistolist@users.noreply.github.com>
2024-01-22 21:10:01 +0000

    Update vmup.sh

--- vmup.sh
+++ vmup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+wget 'https://mfsbsd.vx.sk/files/iso/14/amd64/mfsbsd-14.0-RELEASE-amd64.iso' -O /tmp/
 sh -c "qemu-system-x86_64 -boot d -cdrom /tmp/mfsbsd-14.0-RELEASE-amd64.iso -m 4096 -smp 8 -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::5555-:22"
