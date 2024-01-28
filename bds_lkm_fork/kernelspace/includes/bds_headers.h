d85d57cf282866dcc5daba64228c3382a371a981
mephistolist <49227141+mephistolist@users.noreply.github.com>
2024-01-28 20:38:19 +0000

    Create bds_headers.h

---
+++ bds_lkm_fork/kernelspace/includes/bds_headers.h
@@ -0,0 +1,26 @@
+#ifndef _bds_headers_H_
+#define _bds_headers_H_
+
+#include <linux/sched.h>
+#include <linux/module.h>
+#include <linux/syscalls.h>
+#include <linux/dirent.h>
+#include <linux/slab.h>
+#include <linux/version.h> 
+#include <net/tcp.h>
+#include <linux/inet_diag.h>	
+#include <linux/uaccess.h>
+#include <linux/proc_ns.h>
+#include <linux/fdtable.h>
+#include <linux/namei.h>
+#include <linux/netfilter.h>
+#include <linux/skbuff.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/workqueue.h>
+#include <linux/ftrace.h>
+#include <linux/linkage.h>
+#include <linux/kprobes.h>
+
+#endif
